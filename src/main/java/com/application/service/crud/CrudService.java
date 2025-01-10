fc667b33a4c769829066b14e922f8817652af5d5
mertdeveci <mertdeveci0@gmail.com>
2025-01-10 23:12:40 +0300

    refactor

--- src/main/java/com/application/service/crud/CrudService.java
+++
@@ -1,7 +0,0 @@
-package com.application.service.crud;
-
-import com.application.entity.AbstractEntity;
-
-public interface CrudService<ID, T extends AbstractEntity>
-        extends CreateService<T>, RetrieveService<ID, T>, UpdateService<T>, DeleteService<T> {
-}
