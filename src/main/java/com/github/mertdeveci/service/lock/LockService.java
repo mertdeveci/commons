f654f319789ffb7d842d9b094a82c3f3aa09b9ac
Mert Deveci <mertdeveci0@gmail.com>
2025-01-16 10:13:03 +0300

    Update LockService.java

--- src/main/java/com/github/mertdeveci/service/lock/LockService.java
+++ src/main/java/com/github/mertdeveci/service/lock/LockService.java
@@ -1,10 +1,11 @@
 package com.github.mertdeveci.service.lock;
 
 import com.github.mertdeveci.cache.LockSource;
+import java.time.Duration;
 
 public interface LockService {
-    void lock(String key);
-    void lock(LockSource source, String key);
-    void unlock(String key);
-    void unlock(LockSource source, String key);
+    <T extends LockSource> void lock(LockSource source, String key);
+    <T extends LockSource> void lock(LockSource source, String key, Duration duration);
+    <T extends LockSource> void unlock(LockSource source, String key);
+    <T extends LockSource> void unlock(LockSource source, String key, Duration);
 }
