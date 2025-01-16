36350d0c1ba18df213391e0dff85ba7884898644
mert.deveci <mert.deveci@iyzico.com>
2025-01-16 13:58:05 +0300

    refactors

--- src/main/java/com/github/mertdeveci/service/ExistenceService.java
+++ src/main/java/com/github/mertdeveci/service/ExistenceService.java
@@ -1,5 +1,6 @@
 package com.github.mertdeveci.service;
 
+import com.github.mertdeveci.entity.AbstractEntity;
 import com.github.mertdeveci.exceptions.BusinessException;
 import com.github.mertdeveci.exceptions.business.AlreadyExistsBusinessException;
 import com.github.mertdeveci.exceptions.business.NotFoundBusinessException;
@@ -7,30 +8,30 @@ import com.github.mertdeveci.exceptions.business.NotFoundBusinessException;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-public interface ExistenceService<ID> {
-    Optional<?> isPresent(ID id);
+public interface ExistenceService {
+    <ID extends AbstractEntity> Optional<?> isExists(ID id);
 
-    default boolean exists(ID id){
-        return isPresent(id).isPresent();
+    default <ID extends AbstractEntity> boolean exists(ID id){
+        return isExists(id).isPresent();
     }
 
-    default void exists(ID id, String errorCode){
+    default <ID extends AbstractEntity> void exists(ID id, String errorCode){
         exists(id, () -> { throw new AlreadyExistsBusinessException(errorCode); });
     }
 
-    default <E extends BusinessException> void exists(ID id, Supplier<E> e){
+    default  <ID extends AbstractEntity, E extends BusinessException> void exists(ID id, Supplier<E> e){
         if (!exists(id)){ e.get(); }
     }
 
-    default boolean absent(ID id) {
-        return isPresent(id).isEmpty();
+    default <ID extends AbstractEntity> boolean absent(ID id) {
+        return isExists(id).isEmpty();
     }
 
-    default void absent(ID id, String errorCode) {
+    default <ID extends AbstractEntity> void absent(ID id, String errorCode) {
         absent(id, () -> { throw new NotFoundBusinessException(errorCode); });
     }
 
-    default <E extends BusinessException> void absent(ID id, Supplier<E> e){
+    default <ID extends AbstractEntity, E extends BusinessException> void absent(ID id, Supplier<E> e){
         if (absent(id)){ e.get(); }
     }
 
