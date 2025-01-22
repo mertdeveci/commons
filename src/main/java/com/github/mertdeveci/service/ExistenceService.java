4917845551dcc6a72228fc311c6670dbbda85929
mertdeveci <mertdeveci0@gmail.com>
2025-01-22 15:56:29 +0300

    refactor

--- src/main/java/com/github/mertdeveci/service/ExistenceService.java
+++ src/main/java/com/github/mertdeveci/service/ExistenceService.java
@@ -6,25 +6,25 @@ import com.github.mertdeveci.exceptions.business.NotFoundBusinessException;
 import java.util.function.Supplier;
 
 public interface ExistenceService {
-    boolean isExists(Long id);
+    boolean exists(Long id);
 
-    default <T> T ifExistsDo(Long id, Supplier<T> apply){
-        return isExists(id) ? apply.get() : null;
+    default <T> T ifExistsThenSupply(Long id, Supplier<T> apply){
+        return exists(id) ? apply.get() : null;
     }
 
     default <E extends AlreadyExistsBusinessException> void isAlreadyExists(Long id, Supplier<E> e){
-        if (isExists(id)){ e.get(); }
+        if (exists(id)){ e.get(); }
     }
 
-    default boolean absent(Long id) {
-        return !isExists(id);
+    default boolean notExists(Long id) {
+        return !exists(id);
     }
 
-    default <T> T ifNotFoundDo(Long id, Supplier<T> apply){
-        return absent(id) ? apply.get() : null;
+    default <T> T ifNotExistsThenSupply(Long id, Supplier<T> apply){
+        return notExists(id) ? apply.get() : null;
     }
 
     default <E extends NotFoundBusinessException> void isNotFound(Long id, Supplier<E> e){
-        if (absent(id)){ e.get(); }
+        if (notExists(id)){ e.get(); }
     }
 }
