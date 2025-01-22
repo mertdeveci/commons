57ea2b96e93ef54c603a89bb98fdc0eab861c78f
mertdeveci <mertdeveci0@gmail.com>
2025-01-22 15:04:44 +0300

    refactor

--- src/main/java/com/github/mertdeveci/service/ValidateAndRetrieveService.java
+++ src/main/java/com/github/mertdeveci/service/ValidateAndRetrieveService.java
@@ -5,7 +5,7 @@ import com.github.mertdeveci.service.crud.RetrieveService;
 
 import java.util.Optional;
 
-public interface ValidateAndRetrieveService<T extends AbstractEntity> extends ValidateService, RetrieveService<T> {
+public interface ValidateAndRetrieveService<T extends AbstractEntity> extends ValidateService, RetrieveService {
     default Optional<T> validateAndRetrieve(Object validationObject, Long id){
         isValid(validationObject);
         return retrieveById(id);
