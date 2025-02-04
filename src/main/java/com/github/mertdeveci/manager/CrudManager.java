d87ed67f24de1d6d6b12d97dcff760760ca90984
mertdeveci <mertdeveci0@gmail.com>
2025-02-04 18:28:25 +0300

    refactor

--- src/main/java/com/github/mertdeveci/manager/CrudManager.java
+++ src/main/java/com/github/mertdeveci/manager/CrudManager.java
@@ -1,12 +1,11 @@
 package com.github.mertdeveci.manager;
 
+import com.github.mertdeveci.entity.AbstractEntity;
 import com.github.mertdeveci.service.crud.CreateService;
 import com.github.mertdeveci.service.crud.DeleteService;
 import com.github.mertdeveci.service.crud.RetrieveService;
 import com.github.mertdeveci.service.crud.UpdateService;
 
-public abstract class CrudManager extends RetrieveService implements CreateService, DeleteService, UpdateService {
-    public CrudManager(String defaultErrorMessage) {
-        super(defaultErrorMessage);
-    }
+public interface CrudManager<T extends AbstractEntity>
+        extends CreateService<T>, RetrieveService<T>, UpdateService<T>, DeleteService<T> {
 }
