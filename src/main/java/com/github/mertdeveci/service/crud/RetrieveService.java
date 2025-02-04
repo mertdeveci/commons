b9cd57b0f5d2c0fb3ada6602699bbfd9dda0ef26
mertdeveci <mertdeveci0@gmail.com>
2025-02-04 17:28:22 +0300

    refactor

--- src/main/java/com/github/mertdeveci/service/crud/RetrieveService.java
+++ src/main/java/com/github/mertdeveci/service/crud/RetrieveService.java
@@ -9,19 +9,14 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public abstract class RetrieveService<T extends AbstractEntity> {
-    private final String DEFAULT_ERROR_MESSAGE;
-    public abstract Optional<T> retrieveById(Long id);
+public interface RetrieveService<T extends AbstractEntity> {
+    Optional<T> retrieveById(Long id);
 
-    public RetrieveService(String defaultErrorMessage) {
-        DEFAULT_ERROR_MESSAGE = defaultErrorMessage;
-    }
-
-    public T retrieveByIdOrElseThrow(@Nonnull Long id){
+    default T retrieveByIdOrElseThrow(@Nonnull Long id){
         return retrieveById(id).orElseThrow(() -> new NotFoundBusinessException(DEFAULT_ERROR_MESSAGE));
     }
 
-    public <E extends NotFoundBusinessException> T retrieveByIdOrElseThrow(@Nonnull Long id, @Nonnull ExceptionSupplier<E> e){
+    default <E extends NotFoundBusinessException> T retrieveByIdOrElseThrow(@Nonnull Long id, @Nonnull ExceptionSupplier<E> e){
         Optional<T> entity = retrieveById(id);
         if (entity.isEmpty()) {
             throw e.get();
@@ -29,7 +24,7 @@ public abstract class RetrieveService<T extends AbstractEntity> {
         return entity.get();
     }
 
-    public T retrieveAndValidate(Long id, Consumer<T> validatorConsumer){
+    default T retrieveAndValidate(Long id, Consumer<T> validatorConsumer){
         Optional<T> optional = retrieveById(id);
         if (optional.isEmpty()) {
             return null;
