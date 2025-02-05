db39e733516df0772419309a3a2db9170d3cbb6e
mertdeveci <mertdeveci0@gmail.com>
2025-02-05 14:44:36 +0300

    refactor

--- src/main/java/com/github/mertdeveci/manager/CrudManager.java
+++ src/main/java/com/github/mertdeveci/manager/CrudManager.java
@@ -1,10 +1,28 @@
 package com.github.mertdeveci.manager;
 
+import com.github.mertdeveci.entity.AbstractEntity;
 import com.github.mertdeveci.model.Request;
+import com.github.mertdeveci.model.Response;
+import com.github.mertdeveci.service.crud.CreateService;
+import com.github.mertdeveci.service.crud.DeleteService;
+import com.github.mertdeveci.service.crud.RetrieveService;
+import com.github.mertdeveci.service.crud.UpdateService;
 
-public interface CrudManager {
-    <T extends Request> T create(T request);
-    <T extends Request> T retrieve(T request);
-    <T extends Request> T update(T request);
-    <T extends Request> T delete(T request);
+public abstract class CrudManager<T extends AbstractEntity> {
+    private final RetrieveService<T> retrieveService;
+    private final CreateService<T> createService;
+    private final UpdateService<T> updateService;
+    private final DeleteService<T> deleteService;
+
+    protected CrudManager(RetrieveService<T> retrieveService, CreateService<T> createService, UpdateService<T> updateService, DeleteService<T> deleteService) {
+        this.retrieveService = retrieveService;
+        this.createService = createService;
+        this.updateService = updateService;
+        this.deleteService = deleteService;
+    }
+
+    abstract <K extends Request, L extends Response> K create(K request);
+    abstract <K extends Request, L extends Response> K retrieve(K request);
+    abstract <K extends Request, L extends Response> K update(K request);
+    abstract <K extends Request, L extends Response> K delete(K request);
 }
