b2e544f035c5365a3fd7956ce1f39c82f6aa951f
mertdeveci <mertdeveci0@gmail.com>
2025-02-10 00:57:52 +0300

    refactor

--- src/main/java/com/github/mertdeveci/error/DefaultErrorManager.java
+++ src/main/java/com/github/mertdeveci/error/DefaultErrorManager.java
@@ -3,13 +3,13 @@ package com.github.mertdeveci.error;
 public class DefaultErrorManager {
     private DefaultErrorManager() {}
 
-    public static Error generate(String customErrorCode) {
-        return new Error(customErrorCode);
+    public static DefaultError create(String customErrorCode) {
+        return new DefaultError(customErrorCode);
     }
 
-    public static Error generate(DefaultErrorTemplate errorTemplate, String customErrorCode) {
+    public static DefaultError create(DefaultErrorTemplate errorTemplate, String customErrorCode) {
         String customizedErrorCode = String.format(errorTemplate.getErrorCode(), customErrorCode);
-        return new Error(customizedErrorCode);
+        return new DefaultError(customizedErrorCode);
     }
 
 }
