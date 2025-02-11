7f7c2716bdf28422bb9ebd1288456f2b98ce48a8
mertdeveci <mertdeveci0@gmail.com>
2025-02-11 19:03:07 +0300

    refactor

--- src/main/java/com/github/mertdeveci/request/RequestHandler.java
+++ src/main/java/com/github/mertdeveci/request/RequestHandler.java
@@ -1,10 +1,11 @@
-package com.github.mertdeveci.converter.mapper;
+package com.github.mertdeveci.request;
 
+import com.github.mertdeveci.converter.mapper.Mapper;
 import com.github.mertdeveci.exceptions.validation.InvalidBusinessException;
 import com.github.mertdeveci.functional.ExceptionSupplier;
 import com.github.mertdeveci.service.ValidatorService;
 
-public interface RequestMapper<I, O extends Record> extends Mapper<I, O>, ValidatorService<I> {
+public interface RequestHandler<I, O extends Record> extends Mapper<I, O>, ValidatorService<I> {
 
     default <E extends InvalidBusinessException> O validateAndMap(I request, ExceptionSupplier<E> validationException) {
         this.validate(request, validationException);
